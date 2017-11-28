237266fdbc4b5fa91ffbb57a30251e1ebd4f84a0
fontbyby <lylweb@sina.com>
2017-11-29 00:03:12 +0800

    HMR

--- src/index.js
+++ src/index.js
@@ -1,17 +1,16 @@
-
 import printMe from './print.js';
 
-  function component() {
+function component() {
     var element = document.createElement('div');
     var btn = document.createElement('button');
 
-    element.innerHTML = 'Hello webpack'
+    element.innerHTML = 'Hello - webpack'
     btn.innerHTML = 'Click me and check the console!';
     btn.onclick = printMe;
 
     element.appendChild(btn);
 
     return element;
-  }
+}
 
-  document.body.appendChild(component());
+document.body.appendChild(component());
--- webpack.config.js
+++ webpack.config.js
@@ -44,12 +44,14 @@ module.exports = {
     },
     output: {
         filename: "[name].bundle.js",
-        path: path.resolve(__dirname, "dist")
+        path: path.resolve(__dirname, "dist"),
+        publicPath: "/"
     },
     plugins: [
         new CleanWebpackPlugin(["dist"]),
         new HtmlWebpackPlugin({
             title: "Output Manangement"
         })
-    ]
+    ],
+    devtool: "inline-source-map"
 }
