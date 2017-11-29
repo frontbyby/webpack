a2137943654576db51e2c7e2b77e5dd972a6dcad
fontbyby <lylweb@sina.com>
2017-11-29 23:46:51 +0800

    uglifyPlugin

--- src/index.js
+++ src/index.js
@@ -1,14 +1,20 @@
-import printMe from './print.js';
+// import printMe from './print.js'; 
+import { cube } from './math.js';
 
 function component() {
-    var element = document.createElement('div');
-    var btn = document.createElement('button');
+    // var element = document.createElement('div');
+    // var btn = document.createElement('button');
 
-    element.innerHTML = 'Hello - webpack'
-    btn.innerHTML = 'Click me and check the console!';
-    btn.onclick = printMe;
+    // element.innerHTML = 'Hello - webpack'
+    // btn.innerHTML = 'Click me and check the console!';
+    // btn.onclick = printMe;
 
-    element.appendChild(btn);
+    // element.appendChild(btn);
+    var element = document.createElement('pre');
+    element.innerHTML = [
+        'hello webpack',
+        '5 cube is equal to ' + cube(5)
+    ].join('\n\n');
 
     return element;
 }
--- webpack.config.js
+++ webpack.config.js
@@ -1,49 +1,13 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const CleanWebpackPlugin = require("clean-webpack-plugin");
-/*module.exports = {
-    entry:{"index.js": "./src/index.js"},
-    output: {
-        filename: "bundle.js",
-        path: path.resolve(__dirname, "dist")
-    },
-    module: {
-        rules: [
-            {
-                test: /\.css$/,
-                use: [
-                    "style-loader",
-                    "css-loader"
-                ]
-            },
-            {
-                test: /\.(png|svg|jpg|gif)$/,
-                use: [
-                    "file-loader"
-                ]
-            },
-            {
-                test: /\.(csv|tsv)$/,
-                use: [
-                    "csv-loader"
-                ]
-            },
-            {
-                test: /\.xml$/,
-                use: [
-                    "xml-loader"
-                ]
-            }
-        ]
-    }
-};*/
+const uglifyJSPlugin = require('uglifyjs-webpack-plugin');
 module.exports = {
     entry: {
-        app: "./src/index.js",
-        print: "./src/print.js"
+        app: "./src/index.js"
     },
     output: {
-        filename: "[name].bundle.js",
+        filename: "bundle.js",
         path: path.resolve(__dirname, "dist"),
         publicPath: "/"
     },
@@ -51,7 +15,8 @@ module.exports = {
         new CleanWebpackPlugin(["dist"]),
         new HtmlWebpackPlugin({
             title: "Output Manangement"
-        })
+        }),
+        new uglifyJSPlugin()
     ],
     devtool: "inline-source-map"
 }
